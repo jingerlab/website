aa670bbbd5d524de8d59c5e67ad842fcce605ebe
jingerlab <jingerlab.in@gmail.com>
2026-04-22 11:42:59 +0530

    First push from local

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import pagefind from "astro-pagefind";
+
+export default defineConfig({
+  // The order matters: Tailwind should usually come before React
+  integrations: [react(), pagefind()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
